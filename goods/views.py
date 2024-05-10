8a6c897b4674afb5fbd4af12854a9db6b9a92e98
IlyaBehal <illabegal@gmail.com>
2024-05-10 22:30:02 +0300

    Simple search create, new fixtures before Postgres

--- goods/views.py
+++ goods/views.py
@@ -3,16 +3,20 @@ from django.shortcuts import get_list_or_404, render
 from django.template import context
 
 from goods.models import Products
+from goods.unilits import q_search
 
 
-def catalog(request, category_slug):
+def catalog(request, category_slug=None):
 
     page = request.GET.get('page', 1)
     on_sale = request.GET.get('on_sale', None)
     order_by = request.GET.get('order_by', None)
+    query = request.GET.get('q', None)
 
     if category_slug == 'all':
         goods = Products.objects.all()
+    elif query:
+        goods = q_search(query)
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
 
@@ -21,10 +25,6 @@ def catalog(request, category_slug):
     if order_by and order_by != 'default':
         goods = Products.objects.order_by(order_by)
         
-
-
-
-
     paginator = Paginator(goods, 3)
     current_page = paginator.page(int(page))
 
