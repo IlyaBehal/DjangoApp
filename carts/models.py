76129de6f53a19e5ac6ab7dd05eb421689d06c77
IlyaBehal <illabegal@gmail.com>
2024-05-15 19:14:25 +0300

    Add to cart view< refactor included cart templates

--- carts/models.py
+++ carts/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+from goods.models import Products
+
+class CartsQueryset(models.QuerySet):
+
+    def total_price(self):
+        return sum(cart.products_price() for cart in self)
+    
+    def total_quantity(self):
+        if self:
+            return sum(cart.quantity for cart in self)
+        
+        return 0
+
+class Cart(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, blank=True, null=True)
+    product = models.ForeignKey(to=Products, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField(default=0)
+    session_key = models.CharField(max_length=32, null=True, blank=True)
+    created_timestamp = models.DateTimeField(auto_now_add=True)
+
+    objects = CartsQueryset().as_manager()
+
+    class Meta:
+        db_table = 'cart'
+
+    def products_price(self):
+        return round(self.product.sell_price() * self.quantity,2)
+
+    def __str__(self):
+        return f'Кошик {self.user.username}| товар: {self.product.name} | к-сть: {self.quantity}'
--- carts/urls.py
+++ carts/urls.py
@@ -5,7 +5,7 @@ from carts import views
 app_name = 'carts'
 
 urlpatterns = [
-    path('cart_add/<int:product_id>/', views.cart_add, name='carts_add'),
-    path('cart_change/<int:product_id>/', views.cart_change, name='carts_change'),
-    path('cart_remove/<int:product_id>/', views.cart_remove, name='cart_remove'),
+    path('cart_add/<slug:product_slug>/', views.cart_add, name='carts_add'),
+    path('cart_change/<slug:product_slug>/', views.cart_change, name='carts_change'),
+    path('cart_remove/<slug:product_slug>/', views.cart_remove, name='cart_remove'),
 ]
