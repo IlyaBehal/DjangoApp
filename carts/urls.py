2e7607749937546f3c03717de6228f39ef1f366f
IlyaBehal <illabegal@gmail.com>
2024-05-15 20:58:27 +0300

    Cart romove controller added. Commit before ajax

--- carts/urls.py
+++ carts/urls.py
@@ -7,5 +7,5 @@ app_name = 'carts'
 urlpatterns = [
     path('cart_add/<slug:product_slug>/', views.cart_add, name='carts_add'),
     path('cart_change/<slug:product_slug>/', views.cart_change, name='carts_change'),
-    path('cart_remove/<slug:product_slug>/', views.cart_remove, name='cart_remove'),
+    path('cart_remove/<int:cart_id>/', views.cart_remove, name='cart_remove'),
 ]
